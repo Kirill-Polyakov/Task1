88d6148c4b75dac59bd98da786bd89827046c644
Polyakov Kirll <2696227@mail.ru>
2024-03-14 15:39:13 +0300

    Branch ver 2

--- src/Bank.java
+++ src/Bank.java
@@ -6,6 +6,5 @@ public class Bank {
     public void licSchet(){
         System.out.println(invest(15000, 12, 30));
         System.out.println("Branch 1 completed");
-        System.out.println("Branch 1 + Branch 2 completed");
     }
 }
